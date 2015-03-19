87b853813195c070cc5c852851091882b356400a
tamura shingo <tamura.shingo@gmail.com>
2015-03-20 06:43:17 +0900

    feature/base
        イベント情報をMarkdownで書けるように変更

--- client/app/create/create.controller.js
+++ client/app/create/create.controller.js
@@ -1,13 +1,35 @@
 'use strict';
 
 angular.module('ben33App')
-  .controller('CreateCtrl', ['$scope', '$http', '$interval', '$sce', function($scope, $http, $interval, $sce) {
+  .controller('CreateCtrl', ['$scope', '$sce', 'createService', function($scope, $sce, createService) {
 
-    $interval(function () {
-      $http.post('/api/event/preview', {md: $scope.desc}).then(function(data) {
-        $scope.preview = $sce.trustAsHtml(data.data.html);
+    function concat(scope) {
+      var obj = {
+        eventName: scope.eventName,
+        startDate: scope.startDate,
+        endDate: scope.endDate,
+        venue: scope.venue,
+        mgr: scope.mgr,
+        abstraction: scope.abstraction,
+        desc: scope.desc
+      };
+      var markdown = createService.concatEvent(obj);
+      return markdown;
+    }
+
+    $scope.$watchGroup([
+      'eventName',
+      'startDate',
+      'endDate',
+      'venue',
+      'mgr',
+      'abstraction',
+      'desc'],
+      function(oldVal, newVal) {
+        var markdown = concat($scope);
+        createService.convertMarkdown(markdown)
+          .then(function (html) {
+            $scope.preview = html;
+          });
       });
-    }, 500);
-
   }]);
-
--- client/app/create/create.service.js
+++ client/app/create/create.service.js
@@ -1,17 +1,100 @@
 'use strict';
 
 angular.module('ben33App')
-  .factory('createService', [function () {
+  .factory('createService', ['$q', '$http','$sce', function ($q, $http, $sce) {
 
+/**
+ *
+ * # Event Name
+ * ### <i class="fa fa-calendar"></i> 2015/03/15 19:00 ~ 2015/03/15 21:00  
+ * ### <i class="fa fa-map-marker"></i> 会議室101
+ * ### <i class="fa fa-user"></i> 管理者
+ * - - -
+ *
+ *
+ */
+
+  /**
+   *
+   *
+   */
+  var convertMarkdown = function (raw) {
+    var res = $q.defer();
+    $http.post('/api/event/preview', {md: raw})
+      .then(function (data) {
+        res.resolve($sce.trustAsHtml(data.data.html));
+      });
+
+    return res.promise;
+  };
+
+
+  /**
+   *
+   *
+   */
   var concatEvent = function (desc) {
     var str = '';
+    // イベント名
     if (desc.eventName) {
-      str = '#' + desc.eventName + '#';
+      str = '#' + desc.eventName;
     }
+    // 開始日時
     if (desc.startDate) {
+      str = str + '\n'
+                + '#### <i class="fa fa-calendar"></i> '
+                + desc.startDate;
+    }
+    // 終了日時
+    if (desc.endDate) {
+      if (!desc.startDate) {
+        str = str + '\n'
+                + '#### <i class="fa fa-calendar"></i> ';
+      }
+      str = str + ' ~ '
+                + desc.endDate;
+    }
+    // 場所
+    if (desc.venue) {
+      str = str + '\n'
+                + '#### <i class="fa fa-map-marker"></i> '
+                + desc.venue;
+    }
+    // イベント管理者
+    if (desc.mgr) {
+      str = str + '\n'
+                + '#### <i class="fa fa-user"></i> '
+                + desc.mgr;
     }
 
+    // ハイフネーション
+    str = str + '\n'
+              + '- - -';
+
+    // イベント概要
+    if (desc.abstraction) {
+      str = str + '\n'
+                + desc.abstraction;
+    }
+
+    // ハイフネーション
+    str = str + '\n'
+              + '- - -';
+
+
+    // イベント詳細
+    if (desc.desc) {
+      str = str + '\n'
+                + desc.desc;
+    }
+    return str;
+  };
+
+  return {
+    concatEvent: concatEvent,
+    convertMarkdown: convertMarkdown
   };
 
   }])
 ;
+
