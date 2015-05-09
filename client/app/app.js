e7edbbaf51e4c0e438a22f53ce516d52c575746d
Tamura Shingo <tamura.shingo@gmail.com>
2015-05-10 03:14:50 +0900

    画面遷移時に要ログイン済みかのチェックを行い、必要に応じてログイン画面へリダイレクトする処理を追加
    Authサービスを作成
    
    Userサービスを作成

--- client/app/app.js
+++ client/app/app.js
@@ -13,11 +13,13 @@ angular.module('ben33App', [
   .config(['$stateProvider',
            '$urlRouterProvider',
            '$locationProvider',
+           '$httpProvider',
            'markedProvider',
            'growlProvider',
            function ($stateProvider,
                      $urlRouterProvider,
                      $locationProvider,
+                     $httpProvider,
                      markedProvider,
                      growlProvider) {
              
@@ -25,6 +27,7 @@ angular.module('ben33App', [
                .otherwise('/');
              $.material.init();
              $locationProvider.html5Mode(true);
+             $httpProvider.interceptros.push('authInterceptor');
              
              markedProvider.setOptions({
                gfm: true,
@@ -34,5 +37,51 @@ angular.module('ben33App', [
              growlProvider.onlyUniqueMessages(false);
              growlProvider.globalEnableHtml(true);
 
-           }]);
+           }])
+  .factory('authInterceptor',
+           ['$rootScope',
+            '$q',
+            '$cookieStore',
+            '$location',
+            function ($rootScope, $q, $cookieStore, $location) {
+              return {
+                // Add authorization token to headers
+                request: function (config) {
+                  config.headers = config.headers || {};
+                  if ($cookieStore.get('token')) {
+                    config.headers.Authorization = 'Bearer ' + $cookieStore.get('token');
+                  }
+                  return config;
+                },
+
+                // Intercept 401s and redirect you to login
+                responseError: function (response) {
+                  if (response.status == 401) {
+                    $location.path('/login');
+                    // remove any stale tokens
+                    $cookieStore.remove('token');
+                    return $q.reject(response);
+                  }
+                  else {
+                    return $q.reject(response);
+                  }
+                }
+              };
+            }])
+  .run(['$rootScope',
+        '$location',
+        'Auth',
+        function ($rootScope,
+                  $location,
+                  Auth) {
+          // Redirect to login if route requires auth and you're not logged in
+          $rootScope.$on('$stateChangeStart', function (event, next) {
+            Auth.isLoggedInAsync(function (loggedIn) {
+              if (next.authenticate && !loggedIn) {
+                $location.path('/login');
+              }
+            });
+          });
+        }]);
+
 
